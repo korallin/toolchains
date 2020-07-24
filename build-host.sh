0d38cd70749672a5908274eb4d96ca249655dfe7
Abraão de Santana <abraao.santana@zoop.com.br>
2020-07-23 22:00:58 -0300

    Add warning for deprecated scripts

--- build-host.sh
+++ build-host.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo mkdir -p /opt/cross
-sudo chown $USER /opt/cross
-cd ./crosstool-ng
-./bootstrap
-./configure --prefix=$PWD/../crosstool-ng-build
-make
-make install
-cd ../ctng-workspace-arm-armv6l-linux-gnueabi
-../crosstool-ng-build/bin/ct-ng build
+echo "Re-do! Deprecated!"
+
+#sudo mkdir -p /opt/cross
+#sudo chown $USER /opt/cross
+#cd ./crosstool-ng
+#./bootstrap
+#./configure --prefix=$PWD/../crosstool-ng-build
+#make
+#make install
+#cd ../ctng-workspace-arm-armv6l-linux-gnueabi
+#../crosstool-ng-build/bin/ct-ng build
